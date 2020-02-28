01d27c22d8dc2f3a7ad09bb984c088fcab5bdb59
gitluin <kyluin@outlook.com>
2020-02-27 22:12:43 -0500

    Less obnoxious commenting, some cleaned up code (looking you, string iterating)

--- robco.c
+++ robco.c
@@ -10,30 +10,32 @@
 
 #define DICT_SIZE 17
 
-// Predefine potential words here. Do this on a difficulty-based scale,
-// So we could have three separate arrays, with different words
-// Make sure each array has the same number of entries, DICT_SIZE
+/* Predefine potential words here. Do this on a difficulty-based scale,
+ * So we could have three separate arrays, with different words
+ * Make sure each array has the same number of entries, DICT_SIZE
+ */
 
-// For easy, use words 4-6 chars in length.
-char* easy_words[] = {"test", "panel", "want", "expire", "bases", "pair", "digest", "clone", "steps", "color", "beta", "water", "from", "break",
+/* Easy: 4-6 chars
+ * Med:  6-8 chars
+ * Hard: 8-10 chars
+ */
+const char* easy_words[] = {"test", "panel", "want", "expire", "bases", "pair", "digest", "clone", "steps", "color", "beta", "water", "from", "break",
 			"shift", "space", "relic"};
 
-// For med, use words 6-8 chars in length
-char* med_words[] = {"incubate", "download", "manual", "reagent", "material", "decimal", "pointer", "androids", "trainer", "special", "elated",
+const char* med_words[] = {"incubate", "download", "manual", "reagent", "material", "decimal", "pointer", "androids", "trainer", "special", "elated",
 			"unique", "busted", "include", "satisfy", "mystic", "entered"};
 
-// For hard, use words 8-10 chars in length
-char* hard_words[] = {"sequence", "transform", "technical", "produced", "terminal", "fantastic", "assembly", "buffered", "terminate", "pressure",
+const char* hard_words[] = {"sequence", "transform", "technical", "produced", "terminal", "fantastic", "assembly", "buffered", "terminate", "pressure",
 			"potential", "different", "separate", "relentless", "waiting", "passwords", "emphatic"};
 
 /* Print string one char at a time */
-void disp_step(char* string){
-	int i;
-	for (i=0;string[i] != '\0';i++){
-		printf("%c",string[i]);
-		/* Pause after each char */
+void disp_step(const char* str){
+	while (*str){
+		printf("%c", *str);
 		fflush(stdout);
+		/* Subtle pause */
 		usleep(20000);
+		str++;
 	}
 	/* Dramatic pause */
 	usleep(200000);
@@ -54,47 +56,45 @@ int rand_num(){
 	return rand() % 0xFFFF;
 }
 
-char* choose_password(int difficulty){
+const char* choose_password(int difficulty){
 	int i;
 	time_t t;
 	srand((unsigned) time(&t));
-	if (difficulty == 0){
-		/* don't go over 17 */
-		i = rand() % DICT_SIZE;
+	i = rand() % DICT_SIZE;
+	switch(difficulty){
+	case 0:
 		return easy_words[i];
-
-	} else if (difficulty == 1){
-		i = rand() % DICT_SIZE;
+	case 1:
 		return med_words[i];
-
-	} else if (difficulty == 2){
-		i = rand() % DICT_SIZE;
+	case 2:
 		return hard_words[i];
+	default:
+		break;
 	}
 }
 
-// A function for building a random string, sometimes incorporating a password from the proper difficulty
-// At some point, you need to incorporate the correct password
-// Randomly choose it, and then set a flag when it's done so it doesn't get done again (pass in a pointer to the int in password())
-
+/* A function for building a random string, sometimes incorporating a password from the proper difficulty
+ * At some point, you need to incorporate the correct password
+ * Randomly choose it, and then set a flag when it's done so it doesn't get done again (pass in a pointer to the int in password())
+ */
 
 /* Run the password minigame */
 void password(int difficulty){
-	int success = 0, attempts = 3;
+	int i, success = 0, attempts = 3;
 	/* printed the real password yet? */
-	int i, pass_flag;
+	int pass_flag;
 	int init_hex = rand_num();
-	char* password = choose_password(difficulty);
+	const char* password = choose_password(difficulty);
 
 	header();
 	disp_step("ENTER PASSWORD NOW\n\n");
-	printf("%d", attempts);
-	disp_step(" ATTEMPTS LEFT: @ @ @\n\n");
+	printf("%d", attempts);	disp_step(" ATTEMPTS LEFT: @ @ @\n\n");
 	for (i=0;i<17;i++){ /* 17 is the number of lines the password minigame has */
 		/* first column's hex */
 		printf("0x%X\t", init_hex + i*10);
 		/* 12 chars long */
 		printf("\t");
+		
 		/* Start with the one immediately following the last in the first column */
 		printf("0x%X\t", init_hex + (i+17)*10);
 		/* 12 chars long */
@@ -110,13 +110,13 @@ int main(int argc, char* argv[]){
 		return 0;
 	}
 
-	if (strcmp("easy",argv[1]) == 0){ // if the user entered easy for the difficulty
+	if (strcmp("easy",argv[1]) == 0){
 		difficulty = 0;
 
-	} else if (strcmp("med",argv[1]) == 0){ // else if the user entered med for the difficulty
+	} else if (strcmp("med",argv[1]) == 0){
 		difficulty = 1;
 
-	} else if (strcmp("hard",argv[1]) == 0){ // else if the user entered hard for the difficulty
+	} else if (strcmp("hard",argv[1]) == 0){
 		difficulty = 2;
 
 	} else {
